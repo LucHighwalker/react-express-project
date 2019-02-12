f1bc224db64f727bb5877326bb0136fecad1d1d4
LucHighwalker <luc.highwalker@gmail.com>
2019-02-11 16:12:43 -0800

    - Random numbers and lottery numbers.

--- src/App.js
+++ src/App.js
@@ -7,63 +7,96 @@ import './App.css';
 
 class App extends Component {
   constructor(props) {
-    super(props)
+    super(props);
 
     // State holds values returned from server
     this.state = {
       about: null,
       message: null,
-    }
+      lotto: null,
+      power: null,
+    };
   }
 
   componentDidMount() {
     // Use Fetch to call API. The /test route returns a simple string
     // This call in componentDidMount will only be called once
-    fetch('/about').then((res) => {
-      // stream the response as JSON
-      return res.json()
-    }).then((json) => {
-      console.log(json)
-      const { about } = json // Get a value from JSON object
-      this.setState({ about }) // Set a value on state with returned value
-    }).catch((err) => {
-      // Handle errors
-      console.log(err.message)
-    })
+    fetch('/about')
+      .then((res) => {
+        // stream the response as JSON
+        res.json();
+      })
+      .then((json) => {
+        console.log(json);
+        const { about } = json; // Get a value from JSON object
+        this.setState({ about }); // Set a value on state with returned value
+      })
+      .catch((err) => {
+        // Handle errors
+        console.log(err.message);
+      });
 
     // Let's call another API
-    this.fetchMessage()
+    this.fetchMessage();
   }
 
-  fetchMessage() {
+  formatLotto() {
+    const { lotto } = this.state;
+    if (lotto !== undefined) {
+      let index = -1;
+      return lotto.map((number) => {
+        index += 1;
+        return (
+          <span key={index}>
+            {number}
+            &nbsp;
+          </span>
+        );
+      });
+    }
+    return null;
+  }
+
+  fetchMessage(url = '/random/99') {
     // Wrapping the API call in a function allow you to make calls to this
     // API as often as needed.
-    
-    // This calls a route and passes value in the query string. 
-    fetch('/random/?n=99').then(res => res.json()).then((json) => {
-      console.log(">", json)
-      this.setState({
-        message: json.value,
+
+    // This calls a route and passes value in the query string.
+    fetch(url)
+      .then(res => res.json())
+      .then((json) => {
+        console.log('>', json);
+        this.setState({
+          message: json.value,
+          lotto: json.numbers,
+          power: json.power,
+        });
       })
-    }).catch((err) => {
-      console.log(err.message)
-    })
+      .catch((err) => {
+        console.log(err.message);
+      });
   }
 
   renderMessage() {
     // Used to conditionally render data from server.
     // Returns null if message is null otherwise returns
     // a populated JSX element.
-    const { message } = this.state
+    const { message, power } = this.state;
     if (message === null) {
-      return undefined
+      return undefined;
     }
 
-    return <h1>{message}</h1>
+    return (
+      <div>
+        <h1>{message}</h1>
+        <h1>{this.formatLotto()}</h1>
+        <h1>{power}</h1>
+      </div>
+    );
   }
 
   render() {
-    const { about } = this.state
+    const { about } = this.state;
 
     return (
       <div className="App">
@@ -76,10 +109,18 @@ class App extends Component {
           <button
             type="button"
             onClick={() => {
-              this.fetchMessage()
+              this.fetchMessage();
             }}
           >
-          Random
+            Random Num
+          </button>
+          <button
+            type="button"
+            onClick={() => {
+              this.fetchMessage('/lotto');
+            }}
+          >
+            Random Lotto
           </button>
         </p>
       </div>
